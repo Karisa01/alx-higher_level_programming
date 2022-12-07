968e42a01ed8ed04a11a25363e887f8f40d38af9
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-12-07 03:14:45 -0800

    0x04

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-def multiply_list_map(my_list=[], number=0):
+def multiply_list_values(numbers, factor):
 
-    return list(map(lambda i: i * number, my_list))
+    return [number * factor for number in numbers]
+
+
